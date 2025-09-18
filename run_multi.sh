685b55d39f2ce4f100ecd8128edf14fee10592a9
Afraz Arif Khan <afrazarif@hotmail.com>
2025-09-18 16:43:49 -0700

    created default options

--- run_multi.sh
+++ run_multi.sh
@@ -1,5 +1,5 @@
-output_dir="manuscript_updates"
-data_dir="data/triple_3viruses_nn_data.csv"
+output_dir=""
+data_dir=""
 
 #python train_test.py -o $output_dir -d $data_dir
-python rerun_ml_pipeline.py -o $output_dir -d $data_dir --use_saved_processor "Multiclass/processor.pkl" --use_saved_model "Multiclass/models/nn_model3.h5"
+python rerun_ml_pipeline.py -o $output_dir -d $data_dir 
